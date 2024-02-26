67e98ea5a1651e5db8acb45083a61a3225f900db
Regulr <jeccles2004@gmail.com>
2024-02-26 13:30:33 +0000

    Add project files.

---
+++ Assessment1/Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1
+{
+    internal class Die
+    {
+        public int DieValue { get; set; }
+
+        //Method
+        public void Roll()
+        {
+            Random rdm = new Random();
+            DieValue = rdm.Next(0, 7);
+        }
+
+    }
+}
+
---
+++ Assessment1/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment1
+{
+    internal class Game
+    {
+        /*
+         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
+         *
+         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
+         * rolls could be continous, and the totals and other statistics could be summarised for example.
+         */
+
+        //Methods
+        public void Play()
+        {
+            List<int> dieList = new List<int>();
+            for (int count = 0; count < 3; count++)
+            {
+                Die die = new Die();
+                die.Roll();
+                dieList.Add(die.DieValue);
+            }
+            Console.WriteLine("1." + dieList[0]);
+            Console.WriteLine("2." + dieList[1]);
+            Console.WriteLine("3." + dieList[2]);
+            Console.WriteLine("Overall you rolled a" + dieList.Sum());
+        }
+    }
+}
