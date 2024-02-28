0d5fdd0b063dc6379e77377d5b2fc5a1aa2a6054
Regulr <jeccles2004@gmail.com>
2024-02-28 11:41:06 +0000

    Finished Game - needs comments

--- Assessment1/Program.cs
+++ Assessment1/Program.cs
@@ -14,8 +14,17 @@ namespace Assessment1
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
+
             Testing testing = new Testing();
             testing.Test();
+
+            Game game = new Game();
+            List<int> tempList = game.Play();
+            for (int count = 0; count < tempList.Count; count++)
+            {
+                int dieRoll = count + 1;
+                Console.WriteLine("Die " + dieRoll + " - " + tempList[count]);
+            }
         }
     }
 }
