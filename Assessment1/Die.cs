97223f624bfdcb960044aa856c6cfa63923f5b6d
Regulr <160130042+Regulr@users.noreply.github.com>
2024-03-01 14:35:08 +0000

    Update Die.cs

--- Assessment1/Die.cs
+++ Assessment1/Die.cs
@@ -9,21 +9,11 @@ namespace Assessment1
     internal class Die
     {
         //Property
-        private int _dieValue;
-        public int DieValue 
-        {
-            get => _dieValue; set => _dieValue = value;
-        } 
+        
 
 
         //Method
-        public int Roll()
-        {
-            Random rdm = new Random();
-            //random number between 1 and 6 
-            int num = rdm.Next(1, 7);
-            
-        }
+       
 
     }
 }
