0c991791fc5dc25afd1eecddef20fc157ba4377d
Regulr <160130042+Regulr@users.noreply.github.com>
2024-03-01 14:37:49 +0000

    Update Program.cs

--- Assessment1/Program.cs
+++ Assessment1/Program.cs
@@ -15,23 +15,7 @@ namespace Assessment1
              * Create a Testing object to verify the output and operation of the other classes.
             */
 
-            Testing testing = new Testing();
-            //run tests 
-            testing.Test();
-
-            Game game = new Game();
-            //initialise list based on list returned from game.Play()
-            List<int> tempList = game.Play();
-            //loop through the list and output values 
-            for (int count = 0; count < tempList.Count; count++)
-            {
-                //for each die roll to use in output 
-                int dieRoll = count + 1;
-                //output each Die Roll
-                Console.WriteLine("Die " + dieRoll + " - " + tempList[count]);
-            }
-            //output the sum of all dice rolls 
-            Console.WriteLine("Sum of Dice Rolls - " + tempList.Sum());
+           
         }
     }
 }
